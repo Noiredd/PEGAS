f80ab62dd21748fd11bed5ccf4faab2ed7f1ceac
Noiredd <snowball91b@gmail.com>
2016-02-19 07:53:09 +0100

    cleaned up vector math

--- MATLAB/flightSim3D.m
+++ MATLAB/flightSim3D.m
@@ -7,13 +7,13 @@
 %   [+] basic 3D visualization
 %   [+] thrust
 %   [+] pitch programming
+%   [+] clean up vector math
 %   [ ] drag
 %   [ ] complete data output
 %   [ ] results postprocessing & plotting
 %   [ ] natural gravity turn
 %   [ ] compare 2D vs 3D performance
 %   [ ] reconstruct PEG
-%   [ ] clean up vector math (until this point we'll be doing everything multiple times)
 %   [ ] orbital elements from r&v
 %   [ ] passive yaw programming (verify azimuth~inclination results)
 %   [ ] PEG YAW CONTROL
@@ -55,37 +55,24 @@ function [results] = flightSim3D(vehicle, initial, control, dt)
     pitch = zeros(N,1); %pitch command log [deg] (0 - straight up)
     %vehicle position in cartesian XYZ frame
     r = zeros(N,3);     %from Earth's center [m]
-    rr = zeros(N,1);    %magnitude [m]
+    rmag = zeros(N,1);  %magnitude [m]
     %vehicle velocity
     v = zeros(N,3);     %relative to Earth's center [m/s]
-    vv = zeros(N,1);    %magnitude [m/s]
-    %flight angles (THIS IS PROBABLY INCORRECT FOR NOW)
-    fpa_srf = zeros(1,N);   %Flight Pitch Angle (surface) - how much the vehicle points "up" in relation to moving air [deg]
-                            %at launchpad this is undefined. 0 = straight up, 90 = due East
-    fpa_obt = zeros(1,N);   %the same but related to orbital velocity [deg]le points "up" in relation to moving air [deg]
-                            %at launchpad this equals 90 (only Earth's rotation)
-    fya_srf = zeros(1,N);   %Flight Yaw Angle (surface) - how much the vehicle points "South" in relation to moving air [deg]le points "up" in relation to moving air [deg]
-                            %at launchpad this is undefined (stationary related to surface/air)
-                            %0 = due East, 90 = due South
-    fya_obt = zeros(1,N);   %the same but related to orbital velocity [deg]
-                            %at launchpad this equals 90 (craft does not move towards the equator)
-                            %0 = due East, 90 = due South
+    vmag = zeros(N,1);  %magnitude [m/s]
+    %reference frame matrices
+    nav = zeros(3,3);   %KSP-style navball frame (radial, North, East)
+    rnc = zeros(3,3);   %PEG-style tangential frame (radial, normal, circumferential)
     %forces
-    Ga = [0 0 0];           %current gravitational acceleration vector [m/s^2]
+    Ga = [0 0 0];       %current gravitational acceleration vector [m/s^2]
     
     %SIMULATION INITIALIZATION
     [r(1,1),r(1,2),r(1,3)] = sph2cart(degtorad(initial.lon), degtorad(initial.lat), R+initial.alt);
-    rr(1) = norm(r(1,:));
-    v(1,:) = surfSpeed(r(1,:));
-    vv(1) = norm(v(1,:));
-    ru = r(1,:)/rr(1);
-    nu = cross(r(1,:),v(1,:)); nu = nu/norm(nu);
-    cu = cross(nu,ru);
-    un = [r(1,1) r(1,2) 0]; un = un/norm(un);
-    nn = cross([r(1,1) r(1,2) 0],[v(1,1) v(1,2) 0]); nn = nn/norm(nn);
-    en = cross(nn,un);
+    rmag(1) = norm(r(1,:));
+    v(1,:) = surfSpeedInit(r(1,:));
+    vmag(1) = norm(v(1,:));
+    nav = getNavballFrame(r(1,:), v(1,:));
+    rnc = getCircumFrame(r(1,:), v(1,:));
     
-    temp=zeros(1,N);
     %MAIN LOOP
     for i=2:N
         %PITCH CONTROL
@@ -96,36 +83,22 @@ function [results] = flightSim3D(vehicle, initial, control, dt)
         
         %PHYSICS
         %thrust/acceleration
-        p = approxFromCurve((rr(i-1)-R)/1000, atmpressure);
+        p = approxFromCurve((rmag(i-1)-R)/1000, atmpressure);
         isp = (isp1-isp0)*p+isp0;
         F(i) = isp*g0*dm;
         acc(i) = F(i)/m;
-        acv = acc(i)*cFromNavball(r(i-1,:), v(i-1,:), pitch(i), yaw);
-        %%angle between planar component of thrust and east +angle between coordinate systems in plane
-        %acosd(dot(acv2+acv3,en)/(norm(acv2+acv3)*norm(en)))+acosd(dot(cu,en));
+        acv = acc(i)*makeVector(nav, pitch(i), yaw);
         %gravity
         Ga = mu*r(i-1,:)/norm(r(i-1,:))^3;
         %velocity
         v(i,:) = v(i-1,:) - Ga*dt + acv*dt;
-        vv(i) = norm(v(i,:));
+        vmag(i) = norm(v(i,:));
         %position
         r(i,:) = r(i-1,:) + v(i,:)*dt;
-        rr(i) = norm(r(i,:));
-        %vectors
-            %local coordinates (as used by PEG)
-        ru = r(i,:)/norm(r(i,:));   %radial versor -> our local "Up" (away from the Earth)
-        nu = cross(r(i,:),v(i,:));
-        nu = nu/norm(nu);           %normal versor -> our local "Left"
-        cu = cross(nu,ru);          %circuferential versor -> our local "Forward" (towards the horizon)
-            %"navball" coordinates: not actually what KSP navball looks
-            %like - the real one would use 'ru', 'en' and cross(ru,en)
-        un = [r(i,1) r(i,2) 0];
-        un = un/norm(un);           %local "Equatorial Up"
-        nn = cross([r(i,1) r(i,2) 0],[v(i,1) v(i,2) 0]);
-        nn = nn/norm(nn);           %local "North"
-        en = cross(nn,un);          %local "East"
-            %angles
-            temp(i)=acosd(dot(r(i,:),acv)/(norm(r(i,:))*norm(acv)));
+        rmag(i) = norm(r(i,:));
+        %local reference frames
+        nav = getNavballFrame(r(i,:), v(i,:));
+        rnc = getCircumFrame(r(i,:), v(i,:));
         %MASS&TIME
         m = m - dm*dt;
         t(i) = t(i-1) + dt;
@@ -134,7 +107,7 @@ function [results] = flightSim3D(vehicle, initial, control, dt)
     %OUTPUT
     plots = struct('t', t(1:i),...
                    'v', v,...
-                   'vv', vv,...
+                   'vv', vmag,...
                    'r', r);
     results = struct('Altitude', (norm(r(i,:))-R)/1000,...
                      'Velocity', norm(v(i,:)),...
@@ -150,7 +123,7 @@ function [results] = flightSim3D(vehicle, initial, control, dt)
     sz=sz(11:end,:);
     %comment/uncomment line below to switch between Earth-based plot and trajectory oriented one
     %scale=scale*50;plot3(R*sx,R*sy,R*sz,'g'); scatter3(0,0,0,'g');
-        %lets try a ground path instead a sphere
+        %ground path
         gp=zeros(N,3);
         for i=1:N
             gp(i,:)=R*r(i,:)/norm(r(i,:));
@@ -160,58 +133,105 @@ function [results] = flightSim3D(vehicle, initial, control, dt)
     scatter3(r(i,1),r(i,2),r(i,3),'r');
     scale=scale*50000;
     t=zeros(2,3);
-    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*ru; %radial (away)
+    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*rnc(1,:); %radial (away)
     plot3(t(:,1),t(:,2),t(:,3),'r');
-    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*nu; %normal (plane change)
+    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*rnc(2,:); %normal (plane change)
     plot3(t(:,1),t(:,2),t(:,3),'g');
-    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*cu; %circumferential (prograde)
+    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*rnc(3,:); %circumferential (prograde)
     plot3(t(:,1),t(:,2),t(:,3),'b');
-        %[dot(ru,nu) dot(ru,cu) dot(nu,cu)]
+        rnc
     %navball versors
-    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*un; %away in equatorial plane
+    scale = scale/2;
+    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*nav(1,:); %away in equatorial plane
     plot3(t(:,1),t(:,2),t(:,3),'k');
-    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*nn; %north
+    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*nav(2,:); %north
     plot3(t(:,1),t(:,2),t(:,3),'k');
-    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*en; %east
+    t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*nav(3,:); %east
     plot3(t(:,1),t(:,2),t(:,3),'k');
-        %[dot(un,nn) dot(un,en) dot(nn,en)]
+        nav
     %acceleration
     t(1,:)=r(i,:); t(2,:)=t(1,:)+scale*0.03*acv;%final
     plot3(t(:,1),t(:,2),t(:,3),'y');
     hold off;
+    axis equal;
 end
 
-%constructs a cartesian unit vector for given navball heading
-function [c] = cFromNavball(r, v, p, y)
+%constructs a local reference frame, KSP-navball style
+function [f] = getNavballFrame(r, v)
     %pass current position under r (1x3)
     %current velocity under v (1x3)
-    %pitch aim under p (scalar) - degrees away from Up
-    %yaw aim under y (scalar) - degrees from East towards North
-    eup = [r(1) r(2) 0];
-    eup = eup/norm(eup);        %local "Equatorial Up"
-    north = cross([r(1) r(2) 0],[v(1) v(2) 0]);
-    north = north/norm(north);  %local "North"
-    east = cross(north,eup);    %true "East"
-    up = r/norm(r);             %true "Up" (away from the planet)
-    tn = cross(up,east);        %true "North"
-    %up, north and east components
-    a = cosd(p)*up;
-    b = sind(p)*sind(y)*tn;
-    c = sind(p)*cosd(y)*east;
-    c = a + b + c;
+    pseudo_up = [r(1) r(2) 0]/norm([r(1) r(2) 0]);
+    pseudo_north = cross([r(1) r(2) 0],[v(1) v(2) 0]);
+    pseudo_north = pseudo_north/norm(pseudo_north);
+    east = cross(pseudo_north,pseudo_up);   %true East direction
+    up = r/norm(r);             %true Up direction (radial away from Earth)
+    north = cross(up, east);    %true North direction (completes frame)
+    f = zeros(3,3);
+    %return a right-handed coordinate system base
+    f(1,:) = up;
+    f(2,:) = north;
+    f(3,:) = east;
+end
+
+%constructs a local reference frame in style of PEG coordinate base
+function [f] = getCircumFrame(r, v)
+    %pass current position under r (1x3)
+    %current velocity under v (1x3)
+    radial = r/norm(r);             %Up direction (radial away from Earth)
+    normal = cross(r, v);
+    normal = normal/norm(normal);   %Normal direction (perpendicular to orbital plane)
+    circum = cross(normal, radial); %Circumferential direction (tangential to sphere, in motion plane)
+    f = zeros(3,3);
+    %return a left(?)-handed coordinate system base
+    f(1,:) = radial;
+    f(2,:) = normal;
+    f(3,:) = circum;
+end
+
+%finds rotation angle between the two frames
+function [alpha] = rnc2nav(rnc, nav)
+    %pass reference frame matrices
+    %by their definitions, their 'radial' component is the same, therefore
+    %rotation between them can be described with a single number
+    alpha = dot(rnc(3,:), nav(3,:));
+end
+
+%constructs a unit vector in the global frame for a given azimuth/elevation
+%angles in a given frame (first angle ('p') rotates from frame's first
+%towards second vector, second ('y') rotates from second towards third)
+function [v] = makeVector(frame, p, y)
+    V = zeros(3,3);
+    V(1,:) = cosd(p)*frame(1,:);
+    V(2,:) = sind(p)*sind(y)*frame(2,:);
+    V(3,:) = sind(p)*cosd(y)*frame(3,:);
+    v = V(1,:) + V(2,:) + V(3,:);
+end
+
+%finds Earth's rotation velocity vector at given cartesian location using
+%no velocity vector (meant for rotational velocity initilization)
+function [rot] = surfSpeedInit(r)
+%    global R;
+    %create temporary frame by generating a dummy velocity vector
+    t = r*[0 1 0; -1 0 0; 0 0 1];   %90 degrees counterclockwise around Z axis, looking down
+    f = getNavballFrame(r, t);      %temp frame
+    rot = surfSpeed(r, f);          %use a standard function
+%    %get latitude
+%    [~,lat,~] = cart2sph(r(1), r(2), r(3));
+%    vel = 2*pi*R/(24*3600); %equatorial
+%    vel = vel*cos(lat);     %at latitude
+%    %now, find vector pointing in direction of Earth's rotation
+%    %first rotate our location 90 degrees about Z axis (looking down - counterclockwise)
+%    m = [0 1 0; -1 0 0; 0 0 1];
+%    t = r*m;
+%    %wind blows east - get the vector using a tool we already have
+%    rot = vel*cFromNavball(r, t, 90, 0);
 end
 
 %finds Earth's rotation velocity vector at given cartesian location
-function [rot] = surfSpeed(r)
+%assuming a navball reference frame is available
+function [rot] = surfSpeed(r, nav)
     global R;
-    %get latitude
     [~,lat,~] = cart2sph(r(1), r(2), r(3));
-    vel = 2*pi*R/(24*3600); %equatorial
-    vel = vel*cos(lat);     %at latitude
-    %now, find vector pointing in direction of Earth's rotation
-    %first rotate our location 90 degrees about Z axis
-    m = [0 1 0; -1 0 0; 0 0 1];
-    t = r*m;
-    %wind blows east - get the vector using a tool we already have
-    rot = vel*cFromNavball(r, t, 90, 0);
+    vel = 2*pi*R/(24*3600)*cos(lat);
+    rot = vel*nav(3,:); %third componend is East vector
 end
