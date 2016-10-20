accafe27964fca5c9f4dd8d4076467eedd341997
Noiredd <snowball91b@gmail.com>
2016-10-21 01:12:22 +0200

    increased CSE precision
    Also added adaptive "minimum precision" - in later portion of the flight integrating with a constant step resulted in precision degradation and divergence.
    This enforces a minimum of 100 integration steps, no matter what the delta-t (integration period) is.

--- MATLAB/easyCSE.m
+++ MATLAB/easyCSE.m
@@ -1,8 +1,14 @@
 function [r, v, last] = easyCSE(r0, v0, t, last)
     %naive integration
     global mu;
-    dt = 1;
+    dt = 0.5;
     N = ceil(t/dt);
+    %keep some precision minimum
+    if N<150
+        N = 150;
+        dt = t/N;
+    end
+    
     v=v0;
     r=r0;
     for i=1:N
