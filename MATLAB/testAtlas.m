07ad84bb9379ae9932cfa963f31d825a2cf92c0d
Noiredd <snowball91b@gmail.com>
2016-11-26 21:05:11 +0100

    cleaner test environment

--- MATLAB/testAtlas.m
+++ MATLAB/testAtlas.m
@@ -4,7 +4,10 @@ AtlasV531
 %site = struct('type', 0, 'lat', 28.52406, 'lon', -80.65085, 'alt', 0);   %KSC
 %site = struct('type', 0, 'lat', 34.75083, 'lon', -120.49778, 'alt', 0);  %Vandenberg
 site = struct('type', 0, 'lat', 62.960, 'lon', 40.683, 'alt', 0);        %Plesetsk
-[lan, azm, target] = launchTargeting(site, 250, 90, 2.0);
+
+alt = 200;
+inc = 90;
+[lan, azm, target] = launchTargeting(site, alt, inc, 2.0);
 
 stage1 = struct('type', 0, 'p', 10, 'v', 5, 'a', azm);
 
@@ -14,6 +17,6 @@ telemetry(AV5.powered, AV5.coast, 1);
 dbgIntegrals(AV5.powered(2:AV5.n), 2);
 trajectory(AV5.powered, AV5.coast, target, 2, 1, 3);
 
-fprintf('angle %d\tecc %d\talt %d\n', planeError(AV5, target), AV5.powered(length(AV5.powered)).Orbit.ECC, abs(250-AV5.powered(length(AV5.powered)).Altitude));
+fprintf('angle %d\tecc %d\talt %d\n', planeError(AV5, target), AV5.powered(length(AV5.powered)).Orbit.ECC, abs(alt-AV5.powered(length(AV5.powered)).Altitude));
 
 clearvars site lan azm target stage1
--- MATLAB/testShuttle.m
+++ MATLAB/testShuttle.m
@@ -4,7 +4,10 @@ SpaceShuttle
 site = struct('type', 0, 'lat', 28.52406, 'lon', -80.65085, 'alt', 0);   %KSC
 %site = struct('type', 0, 'lat', 34.75083, 'lon', -120.49778, 'alt', 0);  %Vandenberg
 %site = struct('type', 0, 'lat', 62.960, 'lon', 40.683, 'alt', 0);        %Plesetsk
-[lan, azm, target] = launchTargeting(site, 200, 51.65, 2.0);
+
+alt = 200;
+inc = 51.65;
+[lan, azm, target] = launchTargeting(site, alt, inc, 2.0);
 
 stage1 = struct('type', 0, 'p', 10, 'v', 50, 'a', azm);
 
@@ -14,6 +17,6 @@ telemetry(STS.powered, STS.coast, 1);
 dbgIntegrals(STS.powered(2:STS.n), 2);
 trajectory(STS.powered, STS.coast, target, 2, 1, 3);
 
-fprintf('angle %d\tecc %d\talt %d\n', planeError(STS, target), STS.powered(length(STS.powered)).Orbit.ECC, abs(250-STS.powered(length(STS.powered)).Altitude));
+fprintf('angle %d\tecc %d\talt %d\n', planeError(STS, target), STS.powered(length(STS.powered)).Orbit.ECC, abs(alt-STS.powered(length(STS.powered)).Altitude));
 
 clearvars site lan azm target stage1
