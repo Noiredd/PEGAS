997ea753688d3f98c73107265168a5a1468ac1b8
Noiredd <snowball91b@gmail.com>
2017-02-23 15:33:36 +0100

    updates help and cleans up approxFromCurve

--- MATLAB/approxFromCurve.m
+++ MATLAB/approxFromCurve.m
@@ -1,26 +1,43 @@
-%approxFromCurve.m
-%Given a curve (as a list of 2D points) and an argument (number)
-%approximates a linear function from two points between which the argument
-%is and calculates its output for the number as argument.
 function [y] = approxFromCurve(x, curve)
-    %some safety checks
+%y = APPROXFROMCURVE(x, curve)
+%Interpolates a point on a curve given by list of control points.
+%If the input is between two points of the curve, output will be
+%calculated from the linear function fitted to those points. If the input
+%is out of range, extreme values will be output.
+%
+%INPUT
+%    x          Argument
+%    curve      Array of 2D (XY) points of shape (n,2)
+%
+%OUTPUT
+%   y           Linear approximation for the given argument.
+
     [n,~] = size(curve);
-    %find between which two points defining the curve we are
+    
+    %If the input is outside the given range, output the extreme value.
+    if (x>curve(n,1))
+        y = curve(n,2);
+        return;
+    elseif (x<curve(1,1))
+        y = curve(1,2);
+        return;
+    end;
+    
+    %Find between which two points defining the curve the argument is.
     for i = 2:n
         if curve(i,1) > x
             break;
         end;
     end;
-    %find linear function between those points
-        %m*x1+b=y1
-        %m*x2+b=y2
-        %y1-m*x1 = y2-m*x2
-        %m(x2-x1)=y2-y1
+    %Calculate a linear function coefficients between those points from the
+    %given rearrangement:
+    %    m*x1+b = y1
+    %    m*x2+b = y2
+    %    y1-m*x1  =  y2-m*x2
+    %    m(x2-x1) = y2-y1
+    %    m = (y2-y1)/(x2-x1)
+    %    b = y2 - m*x2
     m = ( curve(i,2)-curve(i-1,2) ) / ( curve(i,1)-curve(i-1,1) );
     b = curve(i,2) - m*curve(i,1);
-    %compute
+    
     y = m*x+b;
-    %if out of range - do nothing, repeat last value to be safe
-    if (x>curve(n,1))
-        y=curve(n,2);
-    end;
